06aed832760eec4619f4ff9e81d806d4a5d18ad0
Abdulrasheed <abdrasheed1103@gmail.com>
2022-02-11 23:07:32 +0100

    Updated date syntax

--- Expense Tracker/src/Components/ExpenseDate.jsx
+++ Expense Tracker/src/Components/ExpenseDate.jsx
@@ -1,6 +1,6 @@
 function ExpenseDate(props) {
   const month = props.date.toLocaleString("en-US", { month: "long" });
-  const day = props.date.toLocaleString("en-US", "2-digit");
+  const day = props.date.toLocaleString("en-GB", "2-digit");
   const year = props.date.getFullYear();
 
   return (
